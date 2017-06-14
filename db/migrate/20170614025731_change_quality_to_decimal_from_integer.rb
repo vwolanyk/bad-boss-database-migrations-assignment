ea3888ef8a4d4b1162d856a474965ae18f7f5411
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-13 23:04:25 -0400

    Change quantity to decimal from integer

---
+++ db/migrate/20170614025731_change_quality_to_decimal_from_integer.rb
@@ -0,0 +1,11 @@
+class ChangeQualityToDecimalFromInteger < ActiveRecord::Migration[5.0]
+  def change
+
+    reversible do |dir|
+      change_table :parts do |t|
+        dir.up   { t.change :quantity, :decimal }
+        dir.down { t.change :quantity, :integer }
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614024853) do
+ActiveRecord::Schema.define(version: 20170614025731) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20170614024853) do
     t.string  "name"
     t.string  "manufacturer"
     t.decimal "cost"
-    t.integer "quantity"
+    t.decimal "quantity"
   end
 
   create_table "widgets", force: :cascade do |t|
