e00a8529d4bb6faa366c5a90a08a3c4b7b291bd1
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-13 22:34:10 -0400

    Add quantity to Parts table

---
+++ db/migrate/20170614023249_add_quantity_to_parts.rb
@@ -0,0 +1,8 @@
+class AddQuantityToParts < ActiveRecord::Migration[5.0]
+  def change
+
+    change_table :parts do |t|
+      t.integer :quantity
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614023012) do
+ActiveRecord::Schema.define(version: 20170614023249) do
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
     t.string  "manufacturer"
     t.decimal "cost"
+    t.integer "quantity"
   end
 
 end
